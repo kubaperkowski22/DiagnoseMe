9142b8a96c56ee4cd95db7f580a7586ac0f12de6
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-10-27 01:09:17 +0200

    dodano model Appointment

---
+++ Models/Appointment.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DiagnoseMe.Models
+{
+    public class Appointment : INotifyPropertyChanged
+    {
+        public int Id { get; set; }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        private string _name;
+        public DateTime DateTime
+        {
+            get => _dateTime;
+            set
+            {
+                _dateTime = value;
+                OnPropertyChanged("DateTime");
+            }
+        }
+        private DateTime _dateTime;
+        public string? Localization
+        {
+            get => _localization;
+            set
+            {
+                _localization = value;
+                OnPropertyChanged("Localization");
+            }
+        }
+        private string? _localization;
+        public bool IsFinished
+        {
+            get => DateTime.UtcNow > DateTime;
+        }
+
+        public Appointment()
+        {
+
+        }
+        public Appointment(string name, DateTime dateTime, string? localization = null)
+        {
+            Name = name;
+            DateTime = dateTime;
+            Localization = localization;
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

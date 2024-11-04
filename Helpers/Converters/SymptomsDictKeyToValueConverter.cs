3f7603b8010c1e5c25fcca1cf5a3641218a76a48
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-11-04 23:20:40 +0100

    konwerter z dict.key na dict.value

---
+++ Helpers/Converters/SymptomsDictKeyToValueConverter.cs
@@ -0,0 +1,45 @@
+﻿using DiagnoseMe.Tools;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace DiagnoseMe.Helpers.Converters
+{
+    public class SymptomsDictKeyToValueConverter : IValueConverter
+    {
+        private Dictionary<string, string> SymptomsDictionary
+        {
+            get
+            {
+                if (_symptomsDictionary is null || _symptomsDictionary.Count == 0)
+                {
+                    var diagnosis = App.Current.Services.GetService<Diagnosis>();
+                    _symptomsDictionary = diagnosis.SymptomsDictionary;
+                }
+
+                return _symptomsDictionary;
+            }
+        }
+        private Dictionary<string, string> _symptomsDictionary;
+
+        public object Convert(object key, Type targetType, object parameter, CultureInfo culture)
+        {
+            string value = SymptomsDictionary[key as string];
+
+            if (value != null)
+                return value;
+            return key; // returns key if could not find value from dictionary
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}

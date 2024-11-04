7eef1e147f92862e54cd435b42b69c75b34ff737
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-11-05 00:24:09 +0100

    dodana klasa SymptomQuestion

---
+++ Tools/SymptomQuestion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnoseMe.Tools
+{
+    public class SymptomQuestion : INotifyPropertyChanged
+    {
+        public string Key { get; set; }
+        public string Question
+        {
+            get => _question;
+            set
+            {
+                if (_question != value)
+                {
+                    _question = value;
+                    OnPropertyChanged(nameof(Question));
+                }
+            }
+        }
+        private string _question;
+
+        public string? Answer
+        {
+            get => _answer;
+            set
+            {
+                if (_answer != value)
+                {
+                    _answer = value;
+                    OnPropertyChanged(nameof(Answer));
+                }
+            }
+        }
+        private string? _answer;
+
+        public SymptomQuestion(string key, string question, string? answer = null)
+        {
+            Key = key;
+            Question = question;
+            Answer = answer;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}

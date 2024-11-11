2ac46f80c5a921ba4f57b881700830606b0a4c71
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-11-11 21:34:03 +0100

    połączenie z AzureDB

---
+++ Tools/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DiagnoseMe.Models;
+
+namespace DiagnoseMe.Tools.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Podaj connection string do bazy danych Azure SQL
+            optionsBuilder.UseSqlServer("REDACTED_CONNECTION_STRING");
+        }
+    }
+}

1e48a1f9d89718facb0b4ca353fe3e343e2c8c54
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-11-11 23:08:07 +0100

    poprawiony widok logowania i rejestracji

---
+++ ViewModels/LogInRegisterVM.cs
@@ -0,0 +1,121 @@
+﻿using DiagnoseMe.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiagnoseMe.ViewModels
+{
+    public class LogInRegisterVM : INotifyPropertyChanged
+    {
+        #region Register
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                _name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+        private string _name;
+
+        public string Surname
+        {
+            get => _surname;
+            set
+            {
+                _surname = value;
+                OnPropertyChanged(nameof(Surname));
+            }
+        }
+        private string _surname;
+
+        public EGender Gender
+        {
+            get => _gender;
+            set
+            {
+                _gender = value;
+                OnPropertyChanged(nameof(Gender));
+            }
+        }
+        private EGender _gender;
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged(nameof(Email));
+            }
+        }
+        private string _email;
+
+        public SecureString Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+        private SecureString _password;
+
+        public SecureString RepeatedPassword
+        {
+            get => _repeatedPassword;
+            set
+            {
+                _repeatedPassword = value;
+                OnPropertyChanged(nameof(RepeatedPassword));
+            }
+        }
+        private SecureString _repeatedPassword;
+
+        #endregion
+
+        #region Log In
+
+        public string LoginEmail
+        {
+            get => _loginEmail;
+            set
+            {
+                _loginEmail = value;
+                OnPropertyChanged(nameof(LoginEmail));
+            }
+        }
+        private string _loginEmail;
+
+        public SecureString LoginPassword
+        {
+            get => _loginPassword;
+            set
+            {
+                _loginPassword = value;
+                OnPropertyChanged(nameof(LoginPassword));
+            }
+        }
+        private SecureString _loginPassword;
+
+        #endregion
+
+        public LogInRegisterVM() 
+        {
+
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Views/Windows/LogInRegisterWindow.xaml.cs
@@ -0,0 +1,93 @@
+﻿using DiagnoseMe.ViewModels;
+using MahApps.Metro.Controls;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using DiagnoseMe.Models;
+using static System.Net.Mime.MediaTypeNames;
+using DiagnoseMe.Helpers;
+
+namespace DiagnoseMe.Views.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AddEventWindow.xaml
+    /// </summary>
+    public partial class LogInRegisterWindow : MetroWindow
+    {
+        public LogInRegisterVM LogInVM
+        {
+            get
+            {
+                return _loginVM;
+            }
+            set
+            {
+                _loginVM = value;
+            }
+        }
+        private LogInRegisterVM _loginVM;
+
+        public LogInRegisterWindow()
+        {
+            InitializeComponent();
+            LogInVM = new LogInRegisterVM();
+            this.DataContext = LogInVM;
+        }
+
+        private void ShowRegisterView_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetRegisterView();
+        }
+
+        private void ShowLogInView_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SetLogInView();
+        }
+
+        private void LogIn_Button_Click(object sender, RoutedEventArgs e)
+        {
+            var mainWindwVM = App.Current.Services.GetService<MainWindowVM>();
+            mainWindwVM.IsUserLoggedIn = true;
+
+            this.Close();
+        }
+
+        private void SetRegisterView()
+        {
+            LogIn_Grid.Visibility = Visibility.Collapsed;
+            Register_Grid.Visibility = Visibility.Visible;
+            Height = 600;
+            Width = 500;
+        }
+
+        private void SetLogInView()
+        {
+            LogIn_Grid.Visibility = Visibility.Visible;
+            Register_Grid.Visibility = Visibility.Collapsed;
+            Height = 320;
+            Width = 480;
+        }
+
+        private void GenderRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (LogInVM is null)
+                return;
+
+            if (Male_RadioButton.IsChecked == true)
+                LogInVM.Gender = EGender.Male;
+            else
+                LogInVM.Gender = EGender.Female;
+        }
+    }
+}

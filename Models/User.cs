731d015fefa0264742b25e3a784c2022ceed51ed
Kuba Perkowski <kubaperkowski22@gmail.com>
2024-11-26 23:28:25 +0100

    primary key dodany do tabeli user, bazka naprawiona

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace DiagnoseMe.Models
 {
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }

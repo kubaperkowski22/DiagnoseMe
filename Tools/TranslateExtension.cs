1e965fd2b77c8f7b896ceaa77885343f5eebf4f2
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-12-08 20:29:49 +0100

    proba 1 - multi language

---
+++ Tools/TranslateExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Markup;
+
+namespace DiagnoseMe.Tools
+{
+    public class TranslateExtension : MarkupExtension
+    {
+        public string Key { get; set; }
+
+        public TranslateExtension(string key)
+        {
+            Key = key;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return LocalizationManager.Instance[Key];
+        }
+    }
+}
